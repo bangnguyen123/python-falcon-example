f4b7a1e957d2c7d29e4c782f17ce6a7c8b8db78b
NguyenBang <comtrang.gioson.giolinh@gmail.com>
2023-12-17 17:09:55 +0700

    add first route, show image source

---
+++ look/images.py
@@ -0,0 +1,10 @@
+import json
+
+import falcon
+
+
+class Resource:
+    def on_get(self, req, resp):
+        doc = {"image": [{"href": "/images/1eaf6ef1-7f2d-4ecc-a8d5-6e8adba7cc0e.png"}]}
+        resp.text = json.dumps(doc, ensure_ascii=False)
+        resp.status = falcon.HTTP_200
