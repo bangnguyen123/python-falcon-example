2e73d1d3decf7b46d9fa1d684d591a05a587ec96
NguyenBang <comtrang.gioson.giolinh@gmail.com>
2023-12-17 17:14:41 +0700

    move a bit to use msgpack instead of json

--- look/images.py
+++ look/images.py
@@ -1,10 +1,11 @@
-import json
-
 import falcon
+
+import msgpack
 
 
 class Resource:
     def on_get(self, req, resp):
         doc = {"image": [{"href": "/images/1eaf6ef1-7f2d-4ecc-a8d5-6e8adba7cc0e.png"}]}
-        resp.text = json.dumps(doc, ensure_ascii=False)
+        resp.data = msgpack.packb(doc, use_bin_type=True)
+        resp.content_type = falcon.MEDIA_MSGPACK
         resp.status = falcon.HTTP_200
