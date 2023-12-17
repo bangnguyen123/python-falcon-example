03d3b8d463645e2cea4bacf073d3bdc7f932ad3d
NguyenBang <comtrang.gioson.giolinh@gmail.com>
2023-12-17 17:50:36 +0700

    add tests

--- look/images.py
+++ look/images.py
@@ -5,7 +5,7 @@ import msgpack
 
 class Resource:
     def on_get(self, req, resp):
-        doc = {"image": [{"href": "/images/1eaf6ef1-7f2d-4ecc-a8d5-6e8adba7cc0e.png"}]}
+        doc = {"images": [{"href": "/images/1eaf6ef1-7f2d-4ecc-a8d5-6e8adba7cc0e.png"}]}
         resp.data = msgpack.packb(doc, use_bin_type=True)
         resp.content_type = falcon.MEDIA_MSGPACK
         resp.status = falcon.HTTP_200
