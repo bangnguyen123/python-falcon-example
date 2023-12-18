f7ea094cc3ca333025128b9e9d90c803d565bfa0
NguyenBang <comtrang.gioson.giolinh@gmail.com>
2023-12-18 23:52:58 +0700

    use dependency injection to create app

--- look/app.py
+++ look/app.py
@@ -1,9 +1,18 @@
+import os
+
 import falcon
 
-from .images import Resource
+from .images import ImageStore, Resource
 
 
-app = application = falcon.App()
+def create_app(image_store):
+    image_resource = Resource(image_store)
+    app = falcon.App()
+    app.add_route("/images", image_resource)
+    return app
 
-images = Resource(storage_path=".")
-app.add_route("/images", images)
+
+def get_app():
+    storage_path = os.environ.get("LOOK_STORAGE_PATH", ".")
+    image_store = ImageStore(storage_path)
+    return create_app(image_store)
